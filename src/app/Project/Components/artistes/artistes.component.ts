49dce55b3be6bdcaed91d71808fa5a29b32b8573
obassols <obassols8@boscdelacoma.cat>
2023-02-14 19:22:21 +0100

    Llista artistes feta

--- src/app/Project/Components/artistes/artistes.component.ts
+++ src/app/Project/Components/artistes/artistes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../../Services/api-service.service';
 
 @Component({
   selector: 'app-artistes',
@@ -9,9 +10,40 @@ export class ArtistesComponent implements OnInit {
 
   quadres = Array<any>();
 
-  constructor() { }
+  constructor(private apiService: ApiServiceService) { }
 
   ngOnInit(): void {
+    this.getQuadres();
   }
 
+  getQuadres(): void {
+    this.apiService.getQuadres(12, 1).subscribe((data: any) => {
+      console.log(data);
+      this.quadres = data.data;
+      this.getArtistes(this.quadres);
+    });
+  }
+
+  getArtistes(quadres: Array<any>): void {
+    quadres.forEach(quadre => {
+      quadre.artista = {
+        nom: '',
+        naixament: '',
+        mort: ''
+      };
+
+      if (quadre.artist_id) {
+        this.apiService.getArtistInfo(quadre.artist_id).subscribe((response: any) => {
+          const data = response.data;
+          quadre.artista.nom = data.title ? data.title : 'Desconegut';
+          quadre.artista.naixament = data.birth_date ? data.birth_date : 'Desconeguda';
+          quadre.artista.mort = data.death_date ? data.death_date : 'Desconeguda';
+        });
+      } else {
+        quadre.artista.nom = 'Desconegut';
+        quadre.artista.naixament = 'Desconeguda';
+        quadre.artista.mort = 'Desconeguda';
+      }
+    });
+  }
 }
